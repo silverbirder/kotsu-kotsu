64bae789b2888958b7833885c11047f4b8e68966
shibamoto <silverbirder@gmail.com>
2024-03-31 13:10:25 +0900

    feat: add app-shell

---
+++ src/app/_components/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  AppShellNavbar,
+  Burger,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function Layout({ children }: Props) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShellHeader>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>Logo</div>
+      </AppShellHeader>
+      <AppShellNavbar p="md">Navbar</AppShellNavbar>
+      <AppShellMain>{children}</AppShellMain>
+    </AppShell>
+  );
+}
