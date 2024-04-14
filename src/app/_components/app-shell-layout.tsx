43d0c8bcfc3d2fe65da24203d3395de4736b1dfc
shibamoto <silverbirder@gmail.com>
2024-04-14 14:35:04 +0900

    chore: change a little layout

--- src/app/_components/app-shell-layout.tsx
+++ src/app/_components/app-shell-layout.tsx
@@ -10,6 +10,7 @@ import {
   Text,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { pagesPath } from "@/lib/$path";
 
 type Props = {
   children: React.ReactNode;
@@ -24,23 +25,30 @@ export function AppShellLayout({ children }: Props) {
       navbar={{
         width: 300,
         breakpoint: "sm",
-        collapsed: { mobile: !opened },
+        collapsed: {
+          desktop: true,
+          mobile: !opened,
+        },
       }}
       padding="md"
     >
-      <AppShellHeader>
+      <AppShellHeader p={"sm"}>
         <Flex gap="md">
           <Burger opened={opened} hiddenFrom="sm" onClick={toggle} size="sm" />
-          <Text
-            fw={900}
-            variant="gradient"
-            gradient={{ from: "teal", to: "orange", deg: 90 }}
-          >
-            Kotsu Kotsu
-          </Text>
+          <a href={pagesPath.$url().path}>
+            <Text
+              fw={900}
+              variant="gradient"
+              gradient={{ from: "teal", to: "orange", deg: 90 }}
+            >
+              こつこつ
+            </Text>
+          </a>
         </Flex>
       </AppShellHeader>
-      <AppShellNavbar p="md">Navbar</AppShellNavbar>
+      <AppShellNavbar p="md">
+        <a href={pagesPath.notebooks.$url().path}>ノートブック一覧</a>
+      </AppShellNavbar>
       <AppShellMain>{children}</AppShellMain>
     </AppShell>
   );
